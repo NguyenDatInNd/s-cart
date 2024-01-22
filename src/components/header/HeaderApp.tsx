2cda7e5e951100ccf55cc03ce76fa41107d85be6
NguyenDatND <ntdt098@gmail.com>
2024-01-22 09:13:47 +0700

    fix search

--- src/components/header/HeaderApp.tsx
+++ src/components/header/HeaderApp.tsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { SearchOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import Image from 'next/image'
 import LanguageSwitch from './LanguageSwitch';
-import { Badge, Modal } from 'antd';
+import { Badge, Drawer, Input, List } from 'antd';
 import { useStoreCart } from '@/store/storeCart';
-import SearchInput from './Search';
+import useStoreShop from '@/store/storeShop';
+import { IProduct } from '@/interfaces';
 
 const HeaderApp: React.FC = () => {
     const storeCart = useStoreCart();
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    const { products, fetchProducts } = useStoreShop();
+
+    useEffect(() => {
+        fetchProducts();
+    }, [fetchProducts]);
+
+    const [searchResults, setSearchResults] = useState<IProduct[]>([]);
+
+    const handleSearch = (value: string) => {
+        const results = products.filter((product) =>
+            product.name.toLowerCase().includes(value.toLowerCase())
+        );
+
+        setSearchResults(results);
+    };
 
     return (
         <header className='sticky z-5 left-0 top-0'>
@@ -44,7 +60,7 @@ const HeaderApp: React.FC = () => {
                 </div>
 
                 <div className='text-3xl w-20 flex justify-between'>
-                    <SearchOutlined onClick={() => setIsModalOpen(true)} className='hover:text-[#d9a1a3] cursor-pointer' />
+                    <SearchOutlined onClick={() => setOpen(true)} className='hover:text-[#d9a1a3] cursor-pointer' />
                     <Badge count={storeCart.order.products.length}>
                         <Link className='text-black' href="/cart">
                             <ShoppingCartOutlined className='hover:text-[#d9a1a3] cursor-pointer text-3xl' />
@@ -53,18 +69,22 @@ const HeaderApp: React.FC = () => {
                 </div>
             </nav>
 
-            <Modal
-                title='Search'
-                open={isModalOpen}
-                onOk={() => setIsModalOpen(false)}
-                onCancel={() => setIsModalOpen(false)}
-                destroyOnClose
-                footer={null}
-            >
-                {isModalOpen && <div className='search-input'>
-                    <SearchInput handleClose={() => setIsModalOpen(false)} />
-                </div>}
-            </Modal>
+            <Drawer title="Tìm kiếm sản phẩm" onClose={() => setOpen(false)} open={open} placement='top' style={{ minHeight: 250 }}>
+                <Input placeholder='Tìm kiếm sản phẩm' prefix={<SearchOutlined />} onChange={(e) => handleSearch(e.target.value)} />
+                <List
+                    dataSource={searchResults}
+                    renderItem={(product) => (
+                        <List.Item onClick={() => setOpen(false)}>
+                            <Link href={`/detail/${product.code.replace(/\s+/g, '-').toLowerCase()}`}>
+                                <div className='flex gap-2 items-center text-base'>
+                                    <Image src={product.src} alt={product.name} width={35} height={25} />
+                                    {product.name}
+                                </div>
+                            </Link>
+                        </List.Item>
+                    )}
+                />
+            </Drawer>
         </header>
     );
 };
